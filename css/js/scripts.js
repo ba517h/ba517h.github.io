43cdd46dfc8d80e5ef83685fb6495c284d263845
Mohammed Basith <mhdbazi@gmail.com>
2016-09-21 18:01:24 +0530

    portfolio 1

---
+++ css/js/scripts.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+
+	/***************** Waypoints ******************/
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInRight');
+	}, {
+		offset: '75%'
+	});
+    
+
+	/***************** Initiate Flexslider ******************/
+	$('.flexslider').flexslider({
+		animation: "slide"
+	});
+
+	/***************** Initiate Fancybox ******************/
+
+	$('.single_image').fancybox({
+		padding: 4,
+	});
+
+	/***************** Tooltips ******************/
+    $('[data-toggle="tooltip"]').tooltip();
+
+	/***************** Nav Transformicon ******************/
+
+	/* When user clicks the Icon */
+	$('.nav-toggle').click(function() {
+		$(this).toggleClass('active');
+		$('.header-nav').toggleClass('open');
+		event.preventDefault();
+	});
+	/* When user clicks a link */
+	$('.header-nav li a').click(function() {
+		$('.nav-toggle').toggleClass('active');
+		$('.header-nav').toggleClass('open');
+
+	});
+
+	/***************** Header BG Scroll ******************/
+    $(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
+    $(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
+	
+	/***************** Smooth Scrolling ******************/
+
+	$(function() {
+
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 2000);
+					return false;
+				}
+			}
+		});
+
+	});
+
+});
